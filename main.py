978cc664bd71575df20dd9800af3a41a279cba19
Alex Castaneda <alexcphys99@gmail.com>
2024-10-22 05:02:45 +0000

    Changes to be committed: 	new file:   2d_trajectories.png 	new file:   3d_trajectories.png 	new file:   __pycache__/bm_plots.cpython-312.pyc 	new file:   __pycache__/msd.cpython-312.pyc 	modified:   main.py 	new file:   msd.png 	modified:   msd.py

--- main.py
+++ main.py
@@ -1,23 +1,18 @@
 import numpy as np
-import math
 import numpy as np
 
-def main():
+from msd import random_walk,MSD_3D
+from bm_plots import plot_3d_tr, plot_2d_tr, plot_msd
+
     
-    num_particles = int(input("Enter the number of particles of your simulation: "))
-    num_frames = int(input("Enter the number of frames of your simulation: "))
-    delta_t = 1.0e-4
+num_particles = int(input("Enter the number of particles of your simulation: "))
+num_frames = int(input("Enter the number of frames of your simulation: "))
+delta_t = 1.0e-4
 
-    r = random_walk(num_particles, num_frames, delta_t)
+r = random_walk(num_particles, num_frames, delta_t)
+MSD = MSD_3D(r, num_particles, num_frames)
+plot_3d_tr(r, 3)
+plot_2d_tr(r, 1)
+plot_msd(MSD, num_frames, delta_t)
 
-if __name__ == '__main__':
-    main()
 
-def random_walk(num_particles, num_frames, delta_t):
-
-    r = np.zeros((3,num_particles, num_frames))
-    
-    for frame in range(1,num_frames):
-        r[:,:,frame] = r[:, :, frame-1] + math.sqrt(2*delta_t)*np.random.normal(0, 1, size = (3, num_particles))
-
-    return r
--- msd.py
+++ msd.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 def MSD_3D(r, num_particles, num_frames):
     displacements = np.zeros((num_particles, num_frames))
@@ -7,3 +8,12 @@ def MSD_3D(r, num_particles, num_frames):
         displacements[:, i] = np.mean(squared_displacement, axis=1)
     MSD = np.mean(displacements, axis=0)
     return MSD
+
+def random_walk(num_particles, num_frames, delta_t):
+
+    r = np.zeros((3,num_particles, num_frames))
+    
+    for frame in range(1,num_frames):
+        r[:,:,frame] = r[:, :, frame-1] + math.sqrt(2*delta_t)*np.random.normal(0, 1, size = (3, num_particles))
+
+    return r
